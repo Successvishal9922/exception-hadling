5f1c26c57ab9bb2a3feca4860c59fb3292b18265
successvishal9922 <success.vishal9922@gmail.com>
2026-02-18 16:17:16 +0530

    first commit

---
+++ exception_handling/divide_by_zero.java
@@ -0,0 +1,19 @@
+package exception_handling;
+
+import java.util.Scanner;
+
+public class divide_by_zero {
+    public static void main(String[] args) {
+        System.out.println("enter A and B");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        try {
+            int c = a / b;
+            System.out.println("Result: " + c);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+    }
+}
